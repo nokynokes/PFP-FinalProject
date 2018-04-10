6781abea89f037e1152e6788b4a6ae24569232b6
Nolan Cretney <nokynokes@gmail.com>
2018-04-10 12:31:42 -0600

    still working on registering mouse clicks properly

--- Fractals.elm
+++ Fractals.elm
@@ -40,7 +40,7 @@ type alias Model =
 
 type Msg =
   SizeUpdated Size
-  -- | Tick Time
+  | Tick Time
   | Click Mouse.Position
 
 init : (Model, Cmd Msg)
@@ -95,31 +95,31 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         SizeUpdated newSize -> {model | window = newSize} ! []  -- ! combines what's after it (multiple commands) into one command message
-        -- Tick time ->
-        --     case model.trees of
-        --         [] -> model ! []
-        --         BranchFamily (start, end) children :: t->
-        --             --takes in previous branch's endpoint and uses as startpoint to help generate its endpoint
-        --             let
-        --                 distOfParent = getDistance (start,end)
-        --                 seed = Random.initialSeed (round time)
-        --                 (power, newSeed1) = Random.step directionGenerator model.seed
-        --                 direction = -1^power
-        --                 (newPt_Scaling, newSeed2) = Random.step pointGenerator (seed)
-        --                 newPt = {x = ((toFloat direction) * newPt_Scaling.x * distOfParent) + end.x , y = (newPt_Scaling.y * distOfParent) + end.y}
-        --                 newTree = BranchFamily (end, newPt) []
-        --
-        --             in
-        --             ( {model | trees = BranchFamily (start, end) (newTree :: children) :: t, seed = newSeed1, time = time}, Cmd.none )
-        --
-        --
-        --         -- branching off of height = 0
-        --
-        --         _ -> Debug.crash "TODO"
+        Tick time ->
+            case model.trees of
+                [] -> model ! []
+                BranchFamily (start, end) children :: t->
+                    --takes in previous branch's endpoint and uses as startpoint to help generate its endpoint
+                    let
+                        distOfParent = getDistance (start,end)
+                        seed = Random.initialSeed (round time)
+                        (power, newSeed1) = Random.step directionGenerator model.seed
+                        direction = -1^power
+                        (newPt_Scaling, newSeed2) = Random.step pointGenerator (seed)
+                        newPt = {x = ((toFloat direction) * newPt_Scaling.x * distOfParent) + end.x , y = (newPt_Scaling.y * distOfParent) + end.y}
+                        newTree = BranchFamily (end, newPt) []
+
+                    in
+                    ( {model | trees = BranchFamily (start, end) (newTree :: children) :: t, seed = newSeed1, time = time}, Cmd.none )
+
+
+                -- branching off of height = 0
+
+                _ -> Debug.crash "TODO"
         Click pos ->
           let
-            start = {x = toFloat pos.x, y = toFloat pos.y}
-            end = {x = toFloat pos.x, y = toFloat (pos.y + 40)}
+            start = {x = (toFloat pos.x), y = (toFloat pos.y)}
+            end = {x = (toFloat pos.x), y = (toFloat (pos.y + 40))}
           in
             { model | trees = (BranchFamily (start,end) []) :: model.trees } ! []
 
@@ -131,7 +131,7 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     batch  -- way to listen for multiple subscriptions
         [ resizes SizeUpdated
-        -- , Time.every (0.5* second) Tick
+        , Time.every (0.5* second) Tick
         , clicks (\pos -> Click pos)
         ]
 
@@ -139,19 +139,19 @@ getWindowSize : Model -> (Int, Int)
 getWindowSize m =
     (m.window.height, m.window.width)
 
-makeBranch : Branch -> Form
-makeBranch (start, end) =
+makeBranch : Branch -> (Float, Float) -> Form
+makeBranch (start, end) (sX, sY) =
 
-   Collage.traced (Collage.solid blue) (Collage.segment (start.x, start.y) (end.x, end.y))
+   Collage.traced (Collage.solid blue) (Collage.segment (start.x * sX, start.y * sY) (end.x * sX, end.y * sY))
 
-getBranches : List Tree -> List Form -> List Form
-getBranches t_lst f_lst =
+getBranches : List Tree -> (Float, Float) -> List Form -> List Form
+getBranches t_lst scales f_lst =
     case t_lst of
         [] -> f_lst
         (BranchFamily (s1, e1) c1) :: t1 ->
             case c1 of
-                [] -> getBranches t1 ( (makeBranch (s1, e1)) :: f_lst)
-                BranchFamily _ _ :: _ -> (getBranches c1  ( (makeBranch (s1, e1)) :: f_lst )) |> getBranches t1
+                [] -> getBranches t1 scales ( (makeBranch (s1, e1) scales) :: f_lst)
+                BranchFamily _ _ :: _ -> (getBranches c1 scales ( (makeBranch (s1, e1) scales) :: f_lst )) |> getBranches t1 scales
                 _ -> Debug.crash "Shouldn't get here"
         _ -> Debug.crash "Shouldn't get here"
 
@@ -164,7 +164,8 @@ view : Model -> Html Msg
 view model =
     let
         (h, w) = getWindowSize model
-        trees = getBranches model.trees []
+        scales = ((toFloat h)/2 , (toFloat w)/2)
+        trees = getBranches model.trees scales []
     in
       Html.div []
           [
